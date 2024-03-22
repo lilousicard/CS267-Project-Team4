72d71960f3c44f996af7ad7eab9077ad9ca30e9a
Lilou Sicard-Noel <lilou.sicard-noel@sjsu.edu>
2024-03-22 13:13:33 -0700

    deleted test file

--- src/testParse.py
+++
@@ -1,6 +0,0 @@
-from nltkParser import words_to_pos_types
-
-# Now you can use words_to_pos_types in anotherFile.py
-sentence = "My name is Lilou Noel."
-pos_sentence = words_to_pos_types(sentence)
-print(pos_sentence)
